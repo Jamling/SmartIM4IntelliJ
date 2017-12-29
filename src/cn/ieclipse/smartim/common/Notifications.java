03f0719ca675d9882549a0e1bab581cb0d0e333b
Jamling <li.jamling@gmail.com>
2017-12-29 09:08:59 +0800

    fix #20

--- src/cn/ieclipse/smartim/common/Notifications.java
+++ src/cn/ieclipse/smartim/common/Notifications.java
@@ -2,6 +2,7 @@ package cn.ieclipse.smartim.common;
 
 import cn.ieclipse.smartim.model.IContact;
 import cn.ieclipse.smartim.views.IMPanel;
+import cn.ieclipse.util.StringUtils;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationType;
@@ -13,7 +14,7 @@ import com.intellij.openapi.ui.popup.Balloon;
 public class Notifications {
     public static void notify(final String title, final CharSequence text) {
         com.intellij.notification.Notifications.Bus.register("SmartIM", NotificationDisplayType.BALLOON);
-        Notification n = new Notification("SmartIM", title, text.toString(), NotificationType.INFORMATION);
+        Notification n = new Notification("SmartIM", title, StringUtils.isEmpty(text) ? "" : text.toString(), NotificationType.INFORMATION);
         com.intellij.notification.Notifications.Bus.notify(n);
     }
 
@@ -21,7 +22,7 @@ public class Notifications {
                               final IContact target, final String title,
                               final CharSequence text) {
         com.intellij.notification.Notifications.Bus.register("SmartIM", NotificationDisplayType.BALLOON);
-        Notification n = new Notification("SmartIM", title, text.toString(), NotificationType.INFORMATION);
+        Notification n = new Notification("SmartIM", title, StringUtils.isEmpty(text) ? "" : text.toString(), NotificationType.INFORMATION);
         com.intellij.notification.Notifications.Bus.notify(n);
     }
 }

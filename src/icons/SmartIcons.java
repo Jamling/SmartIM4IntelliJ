6d2aaa8958917b64f829ee1bfd80091587b43059
Jamling <li.jamling@gmail.com>
2019-04-28 09:39:59 +0800

    change sign-out to png

--- src/icons/SmartIcons.java
+++ src/icons/SmartIcons.java
@@ -10,7 +10,7 @@ import javax.swing.*;
  */
 public class SmartIcons {
     public static Icon signin = IconLoader.getIcon("/icons/sign-in.svg");
-    public static Icon signout = IconLoader.getIcon("/icons/sign-out.svg");
+    public static Icon signout = IconLoader.getIcon("/icons/sign-out.png");
     public static Icon test = IconLoader.getIcon("/icons/gitlab.png");
     public static Icon close = IconLoader.getIcon("/icons/close.svg");
     public static Icon show = IconLoader.getIcon("/icons/eye.svg");

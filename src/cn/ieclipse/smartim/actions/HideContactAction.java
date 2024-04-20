9e7abdd4738faff6a58d821971dc658f475f2041
Jamling <li.jamling@gmail.com>
2024-04-20 11:45:53 +0800

    fix #93

--- src/cn/ieclipse/smartim/actions/HideContactAction.java
+++ src/cn/ieclipse/smartim/actions/HideContactAction.java
@@ -1,9 +1,11 @@
 package cn.ieclipse.smartim.actions;
 
 import cn.ieclipse.smartim.views.IMPanel;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import icons.SmartIcons;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Jamling on 2017/7/12.
@@ -14,6 +16,11 @@ public class HideContactAction extends ToggleAction {
     public HideContactAction(IMPanel panel) {
         super("Hide Conatct", "Hide Contact Tree", SmartIcons.hide);
         this.panel = panel;
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
     }
 
     @Override public boolean isSelected(AnActionEvent anActionEvent) {
--- src/cn/ieclipse/smartim/actions/ScrollLockAction.java
+++ src/cn/ieclipse/smartim/actions/ScrollLockAction.java
@@ -1,9 +1,11 @@
 package cn.ieclipse.smartim.actions;
 
 import cn.ieclipse.smartim.console.IMChatConsole;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.ui.ToggleActionButton;
 import icons.SmartIcons;
+import org.jetbrains.annotations.NotNull;
 
 public class ScrollLockAction extends ToggleActionButton {
     IMChatConsole console;
@@ -11,6 +13,11 @@ public class ScrollLockAction extends ToggleActionButton {
     public ScrollLockAction(IMChatConsole console) {
         super("禁止滚动", SmartIcons.lock);
         this.console = console;
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
     }
 
     @Override public boolean isSelected(AnActionEvent anActionEvent) {
